010e224abef26a3932f8378cf63801f5fc27d41f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-11 17:51:18 +1000

    🎉 Update

--- services/cms/config/database.js
+++ services/cms/config/database.js
@@ -1,13 +1,17 @@
-module.exports = ({ env }) => ({
-    connection: {
-        client: "postgres",
+module.exports = ({ env }) => {
+    console.log(env("DATABASE_HOST", ""));
+
+    return {
         connection: {
-            host: env("DATABASE_HOST", ""),
-            port: env.int("DATABASE_PORT", 0),
-            database: env("DATABASE_NAME", ""),
-            user: env("DATABASE_USER", ""),
-            password: env("DATABASE_PASSWORD", ""),
+            client: "postgres",
+            connection: {
+                host: env("DATABASE_HOST", ""),
+                port: env.int("DATABASE_PORT", 0),
+                database: env("DATABASE_NAME", ""),
+                user: env("DATABASE_USER", ""),
+                password: env("DATABASE_PASSWORD", ""),
+            },
+            useNullAsDefault: true,
         },
-        useNullAsDefault: true,
-    },
-});
+    };
+};
