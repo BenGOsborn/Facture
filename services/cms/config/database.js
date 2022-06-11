98813c198b04b676d3b735b31ce885589fc2ba2b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-11 18:28:44 +1000

    🎉 Update

--- services/cms/config/database.js
+++ services/cms/config/database.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => {
         connection: {
             client: "postgres",
             connection: {
-                host: env("DATABASE_HOST", ""),
-                port: env.int("DATABASE_PORT", 0),
-                database: env("DATABASE_NAME", ""),
-                user: env("DATABASE_USER", ""),
+                host: env("DATABASE_HOST", "127.0.0.1"),
+                port: env.int("DATABASE_PORT", 5432),
+                database: env("DATABASE_NAME", "strapi"),
+                user: env("DATABASE_USER", "postgres"),
                 password: env("DATABASE_PASSWORD", ""),
             },
             useNullAsDefault: true,
