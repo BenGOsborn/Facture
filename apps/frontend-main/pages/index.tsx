3d52a7dd80d0c590179b8ccb85fd6de224f06f96
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-12 21:21:32 +1000

    🎉 Update

--- apps/frontend-main/pages/index.tsx
+++ apps/frontend-main/pages/index.tsx
@@ -1,5 +1,39 @@
-export function Index() {
-    return <h1 className="text-blue-200">Hello</h1>;
+import { GetStaticProps } from "next";
+
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+interface Props {
+    launches: any[];
+}
+
+export function Index({ launches }: Props) {
+    return (
+        <h1>
+            {launches.map((launch, index) => (
+                <div key={index}>{launch.id}</div>
+            ))}
+        </h1>
+    );
 }
 
 export default Index;
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+    const client = new ApolloClient({
+        uri: "https://api.spacex.land/graphql/",
+        cache: new InMemoryCache(),
+    });
+
+    const { data } = await client.query({
+        query: gql`
+            query GetLaunches {
+                launchesPast(limit: 10) {
+                    id
+                    mission_name
+                }
+            }
+        `,
+    });
+
+    return { props: { launches: data.launchesPast } };
+};
