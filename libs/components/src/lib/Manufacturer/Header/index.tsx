5570d43dfb721d11b51c91b5920352cee7d0c7a4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-13 20:15:03 +1000

    🎉 Update

--- libs/components/src/lib/Manufacturer/Header/index.tsx
+++ libs/components/src/lib/Manufacturer/Header/index.tsx
@@ -14,25 +14,23 @@ interface Props {
 export function Header({ name, logo, description, type, thumbnail }: Props) {
     return (
         <header className="pb-6 mb-6 border-b border-gray-200">
-            <div className="flex justify-between mb-6">
-                <div className="w-2/3">
-                    <Image src={logo.data.attributes.url} width={logo.data.attributes.width} height={logo.data.attributes.height} />
-                </div>
-                <div className="flex flex-col">
-                    <h1 className="text-3xl font-bold text-indigo-600 mb-3 ml-auto">{name}</h1>
-                    <p className="text-gray-900 text-md w-3/5 text-right ml-auto">{description}</p>
-                </div>
-            </div>
             <div className="mb-6">
-                <Type types={type} />
-            </div>
-            <div className="flex justify-center">
                 <Image
-                    className="rounded-md"
+                    className="rounded-sm"
                     src={thumbnail?.data?.attributes?.url}
                     width={thumbnail?.data?.attributes?.width}
                     height={thumbnail?.data?.attributes?.height}
                 />
+            </div>
+            <div className="mb-6">
+                <Image src={logo.data.attributes.url} width={logo.data.attributes.width} height={logo.data.attributes.height} />
+            </div>
+            <div className="flex flex-col mb-6">
+                <h1 className="text-3xl font-bold text-indigo-600 mb-3">{name}</h1>
+                <p className="text-gray-900 text-md w-3/5">{description}</p>
+            </div>
+            <div className="mb-6">
+                <Type types={type} />
             </div>
         </header>
     );
