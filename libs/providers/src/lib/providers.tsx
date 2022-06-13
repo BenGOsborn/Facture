e1265fd0b54cb75077f266c368a94df529c39f8b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-13 14:53:30 +1000

    🎉 Update

--- libs/providers/src/lib/providers.tsx
+++ libs/providers/src/lib/providers.tsx
@@ -1,11 +1,9 @@
-import styles from "./providers.module.css";
-
 /* eslint-disable-next-line */
 export interface ProvidersProps {}
 
 export function Providers(props: ProvidersProps) {
     return (
-        <div className={styles["container"]}>
+        <div className="bg-blue-300">
             <h1>Welcome to Providers!</h1>
         </div>
     );
