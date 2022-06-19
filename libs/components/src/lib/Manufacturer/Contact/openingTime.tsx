a10945eb3193badddcb1b268f7bc4d1c90fd169f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-19 19:10:02 +1000

    🎉 Update

--- libs/components/src/lib/Manufacturer/Contact/openingTime.tsx
+++ libs/components/src/lib/Manufacturer/Contact/openingTime.tsx
@@ -1,6 +1,8 @@
 import { FindManufacturerQuery } from "@facture/types";
-import { formatTime, groupOpeningTime, mappingToDay } from "@facture/helpers";
+import { groupOpeningTime } from "@facture/helpers";
 import { Clock } from "tabler-icons-react";
+
+import OpeningTimeIndividual from "./openingTimeIndividual";
 
 interface Props {
     openingTime: FindManufacturerQuery["manufacturers"]["data"][number]["attributes"]["openingTime"];
@@ -15,16 +17,11 @@ export function OpeningTime({ openingTime }: Props) {
                         <Clock color="#1f2937" />
                         <h3 className="font-bold text-lg text-gray-800">Opening Time</h3>
                     </div>
-                    <ul>
+                    <ul className="space-y-1">
                         {Object.entries(groupOpeningTime(openingTime)).map((openingTime, index) => {
                             return (
                                 <li key={index}>
-                                    <span className="font-medium text-gray-800">{mappingToDay(parseInt(openingTime[0]))}</span>:{" "}
-                                    <span className="text-gray-700">
-                                        {openingTime[1]
-                                            .map((openingTime, index) => `${formatTime(openingTime.openTime)} - ${formatTime(openingTime.closeTime)}`)
-                                            .join(", ")}
-                                    </span>
+                                    <OpeningTimeIndividual openingTime={openingTime} />
                                 </li>
                             );
                         })}
---
+++ libs/components/src/lib/Manufacturer/Contact/openingTimeIndividual.tsx
@@ -0,0 +1,25 @@
+import { FindManufacturerQuery } from "@facture/types";
+import { formatTime, mappingToDay } from "@facture/helpers";
+
+interface Props {
+    openingTime: [
+        string,
+        {
+            openTime: any;
+            closeTime: any;
+        }[]
+    ];
+}
+
+export function OpeningTimeIndividual({ openingTime }: Props) {
+    return (
+        <>
+            <span className="font-medium text-gray-800">{mappingToDay(parseInt(openingTime[0]))}</span>:{" "}
+            <span className="text-gray-700">
+                {openingTime[1].map((openingTime) => `${formatTime(openingTime.openTime)} - ${formatTime(openingTime.closeTime)}`).join(", ")}
+            </span>
+        </>
+    );
+}
+
+export default OpeningTimeIndividual;
