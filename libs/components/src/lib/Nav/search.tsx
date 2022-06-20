1d76fff279b82d2d96f5573ce3445913abe103be
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-20 15:23:28 +1000

    🎉 Update

--- libs/components/src/lib/Nav/search.tsx
+++ libs/components/src/lib/Nav/search.tsx
@@ -4,11 +4,11 @@ import { Search as SearchIcon } from "tabler-icons-react";
 export function Search() {
     return (
         <Link href="/">
-            <a className="flex items-center space-x-3 bg-gray-900 px-4 py-2 rounded-md text-lg font-medium text-white cursor-pointer">
+            <a className="flex items-center space-x-3 text-white bg-gray-900 px-4 py-2 rounded-md">
                 <span>
                     <SearchIcon />
                 </span>
-                <span>Search</span>
+                <span className="text-lg font-medium">Search</span>
             </a>
         </Link>
     );
