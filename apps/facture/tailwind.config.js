0f2de3284566a70552d98db2255b748040ca985d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-20 21:54:07 +1000

    🎉 Update

--- apps/facture/tailwind.config.js
+++ apps/facture/tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
     mode: "jit",
 };
