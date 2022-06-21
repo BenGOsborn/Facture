3263ed39e9137ba84133e592fc4e41ab1c796608
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 17:37:31 +1000

    🎉 Update

--- libs/components/src/lib/Manufacturer/index.tsx
+++ libs/components/src/lib/Manufacturer/index.tsx
@@ -15,22 +15,28 @@ export function ManufacturerDisplay({ manufacturer }: Props) {
     const slogan = manufacturer.slogan;
     const dateEstablished = manufacturer.dateEstablished;
     const logo = manufacturer.logo.data.attributes;
-    const description = manufacturer.descriptionShort;
+    const descriptionShort = manufacturer.descriptionShort;
     const type = manufacturer.type.map((type) => type.type);
     const color = manufacturer.color;
     const email = manufacturer.email;
+    const phoneNo = manufacturer.phoneNo;
+    const openingTime = manufacturer.openingTime;
+    const social = manufacturer.social;
     const location = manufacturer.location;
+    const descriptionLong = manufacturer.descriptionLong;
+    const thumbnail = manufacturer.thumbnail;
+    const display = manufacturer.display;
 
     return (
         <div className="flex flex-col lg:flex-row lg:space-x-14 space-y-7 lg:space-y-0 items-start">
             <div className="space-y-7 lg:w-4/5">
-                <Info name={name} slogan={slogan} dateEstablished={dateEstablished} logo={logo} description={description} type={type} color={color} />
-                <Contact email={manufacturer.email} phone={manufacturer.phoneNo} openingTime={manufacturer.openingTime} social={manufacturer.social} />
-                <Location location={manufacturer.location} />
+                <Info name={name} slogan={slogan} dateEstablished={dateEstablished} logo={logo} description={descriptionShort} type={type} color={color} />
+                <Contact email={email} phone={phoneNo} openingTime={openingTime} social={social} />
+                <Location location={location} />
             </div>
             <div className="space-y-7">
-                <Content description={manufacturer.descriptionLong} thumbnail={manufacturer.thumbnail} />
-                <Display display={manufacturer.display} />
+                <Content description={descriptionLong} thumbnail={thumbnail} />
+                <Display display={display} />
             </div>
         </div>
     );
