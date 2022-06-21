8d4a6c171f56d84bb33cf7bad619c7b55bfdf258
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 12:02:14 +1000

    🎉 Update

--- libs/components/src/lib/Landing/Hero/index.tsx
+++ libs/components/src/lib/Landing/Hero/index.tsx
@@ -1,11 +1,11 @@
+import Title from "./title";
+
 interface Props {}
 
 export function Hero({}: Props) {
     return (
         <header className="p-6 bg-white rounded-md shadow-md flex flex-col items-center text-center">
-            <h1 className="text-6xl sm:text-7xl font-extrabold tracking-tight text-gray-900">
-                Find A <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-600 to-blue-400">Manufacturer</span>
-            </h1>
+            <Title />
         </header>
     );
 }
--- libs/components/src/lib/Landing/Hero/title.tsx
+++ libs/components/src/lib/Landing/Hero/title.tsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 interface Props {}
 
 export function Title({}: Props) {
-    const [stage, setStage] = useState<number>(0);
-
-    // **** We will use framer motion to animate this color change
-
-    useEffect(() => {
-        setInterval(() => setStage((prev) => prev + 1), 1000);
-    }, []);
-
     return (
-        <header className="p-6 bg-white rounded-md shadow-md flex flex-col items-center text-center">
-            <h1 className="text-6xl sm:text-7xl font-extrabold tracking-tight text-gray-900">
-                Find A <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-600 to-blue-400">Manufacturer</span>
-            </h1>
-        </header>
+        <h1 className="text-6xl sm:text-7xl font-extrabold tracking-tight text-gray-900">
+            Find A{" "}
+            <motion.span
+                animate={{ color: ["#dc2626", "#ea580c", "#ca8a04", "#16a34a", "#0284c7", "#4f46e5", "#7c3aed", "#c026d3", "#be123c"] }}
+                transition={{ duration: 20, repeat: Infinity, repeatType: "reverse" }}
+            >
+                Manufacturer
+            </motion.span>
+        </h1>
     );
 }
 
