0ad74e03d430986550482ea55aae374b16c08259
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-21 10:20:39 +1000

    🎉 Update

--- apps/facture/layout/index.tsx
+++ apps/facture/layout/index.tsx
@@ -1,7 +1,11 @@
 import { Footer, Nav } from "@facture/components";
 import { GraphQLProvider } from "@facture/providers";
 
-export default function Layout({ children }: { children: any }) {
+interface Props {
+    children: any;
+}
+
+export default function Layout({ children }: Props) {
     return (
         <GraphQLProvider>
             <Nav />
