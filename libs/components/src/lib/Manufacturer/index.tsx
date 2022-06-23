f84d79a9c0492444f28e3cac7fb894ac59601762
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-23 11:21:41 +1000

    🎉 Update

--- libs/components/src/lib/Manufacturer/index.tsx
+++ libs/components/src/lib/Manufacturer/index.tsx
@@ -1,4 +1,5 @@
 import { ManufacturerDisplay } from "@facture/types";
+import { ManufacturerHead } from "../Head";
 
 import { Contact } from "./Contact";
 import { Content } from "./Content";
@@ -12,25 +13,28 @@ interface Props {
 
 export function Manufacturer({ manufacturer }: Props) {
     return (
-        <div className="flex flex-col lg:flex-row lg:space-x-14 space-y-7 lg:space-y-0 items-start">
-            <div className="space-y-7 lg:w-4/5">
-                <Info
-                    name={manufacturer.name}
-                    slogan={manufacturer.slogan}
-                    dateEstablished={manufacturer.dateEstablished}
-                    logo={manufacturer.logo}
-                    description={manufacturer.descriptionShort}
-                    type={manufacturer.type}
-                    color={manufacturer.color}
-                />
-                <Contact email={manufacturer.email} phone={manufacturer.phoneNo} openingTime={manufacturer.openingTime} social={manufacturer.social} />
-                <Location location={manufacturer.location} />
+        <>
+            <ManufacturerHead name={manufacturer.name} manufacturer={} description={manufacturer.descriptionShort} color={manufacturer.color} />
+            <div className="flex flex-col lg:flex-row lg:space-x-14 space-y-7 lg:space-y-0 items-start">
+                <div className="space-y-7 lg:w-4/5">
+                    <Info
+                        name={manufacturer.name}
+                        slogan={manufacturer.slogan}
+                        dateEstablished={manufacturer.dateEstablished}
+                        logo={manufacturer.logo}
+                        description={manufacturer.descriptionShort}
+                        type={manufacturer.type}
+                        color={manufacturer.color}
+                    />
+                    <Contact email={manufacturer.email} phone={manufacturer.phoneNo} openingTime={manufacturer.openingTime} social={manufacturer.social} />
+                    <Location location={manufacturer.location} />
+                </div>
+                <div className="space-y-7">
+                    <Content description={manufacturer.descriptionLong} thumbnail={manufacturer.thumbnail} />
+                    <Display display={manufacturer.display} />
+                </div>
             </div>
-            <div className="space-y-7">
-                <Content description={manufacturer.descriptionLong} thumbnail={manufacturer.thumbnail} />
-                <Display display={manufacturer.display} />
-            </div>
-        </div>
+        </>
     );
 }
 
