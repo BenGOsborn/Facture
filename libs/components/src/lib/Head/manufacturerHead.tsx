8f572eb5e7fbcc5052ed71721d8516df792c661f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-23 11:20:18 +1000

    🎉 Update

--- libs/components/src/lib/Head/manufacturerHead.tsx
+++ libs/components/src/lib/Head/manufacturerHead.tsx
@@ -13,31 +13,31 @@ export function ManufacturerHead({ manufacturer, name, description, color }: Pro
     const url = `https://facture.com.au/manufacturers/${manufacturer}`;
 
     const colorMapping: { [key in ColorDisplay]: string } = {
-        slate: "",
-        gray: "",
-        zinc: "",
-        neutral: "",
-        stone: "",
-        red: "",
-        orange: "",
-        amber: "",
-        yellow: "",
-        lime: "",
-        green: "",
-        emerald: "",
-        teal: "",
-        cyan: "",
-        sky: "",
-        blue: "",
-        indigo: "",
-        violet: "",
-        purple: "",
-        fuchsia: "",
-        pink: "",
-        rose: "",
+        slate: "#475569",
+        gray: "#4b5563",
+        zinc: "#52525b",
+        neutral: "#525252",
+        stone: "#57534e",
+        red: "#dc2626",
+        orange: "#ea580c",
+        amber: "#d97706",
+        yellow: "#ca8a04",
+        lime: "#65a30d",
+        green: "#16a34a",
+        emerald: "#059669",
+        teal: "#0d9488",
+        cyan: "#0891b2",
+        sky: "#0284c7",
+        blue: "#2563eb",
+        indigo: "#4f46e5",
+        violet: "#7c3aed",
+        purple: "#9333ea",
+        fuchsia: "#c026d3",
+        pink: "#db2777",
+        rose: "#e11d48",
     };
 
-    const themeColor = "#daa041"; // **** It would be really cool to map the colours...
+    const themeColor = colorMapping[color];
 
     return (
         <Head>
