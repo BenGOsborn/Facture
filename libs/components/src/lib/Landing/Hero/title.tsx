295c2afabdcce3266e769c3d2505b190e97c3b3b
Ben Osborn <58761920+BenGOsborn@users.noreply.github.com>
2022-06-24 22:28:17 +1000

    Update from extrabold to bold

--- libs/components/src/lib/Landing/Hero/title.tsx
+++ libs/components/src/lib/Landing/Hero/title.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export function Title() {
     return (
-        <h1 className="text-5xl sm:text-7xl font-extrabold tracking-tight text-gray-900">
+        <h1 className="text-5xl sm:text-7xl font-bold tracking-tight text-gray-900">
             Find A{" "}
             <motion.span
                 animate={{ color: ["#dc2626", "#ea580c", "#ca8a04", "#16a34a", "#0284c7", "#4f46e5", "#7c3aed", "#c026d3", "#be123c"] }}
