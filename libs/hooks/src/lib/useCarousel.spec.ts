ea8bca1b0929fe64fa55b92ae0bf3d31e947f729
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-25 07:49:55 +1000

    🎉 Update

--- libs/hooks/src/lib/useCarousel.spec.ts
+++ libs/hooks/src/lib/useCarousel.spec.ts
@@ -1,3 +1,12 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useCarousel } from "./useCarousel";
+
 describe("use carousel", () => {
-    it("should cycle between the different elements correctly", () => {});
+    it("should cycle between the different elements correctly", () => {
+        const { result } = renderHook(() => useCarousel(3, 1000));
+
+        expect(result.current.selected).toEqual(0);
+
+        // act(() => result.current.);
+    });
 });
