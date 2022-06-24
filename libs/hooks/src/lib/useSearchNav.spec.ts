5c36de70d02cb6a9717dadb60c7313b387c4d7a4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-25 08:32:36 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchNav.spec.ts
+++ libs/hooks/src/lib/useSearchNav.spec.ts
@@ -1,42 +1,41 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import { useSearchNav } from "./useSearchNav";
 
-jest.mock("algoliasearch", () => {
-    return (algoliaAppId: string, algoliaApiKey: string) => {
+jest.mock("algoliasearch", () => ({
+    default: (algoliaAppId: string, algoliaApiKey: string) => {
         return {
             initIndex: (algoliaIndexName: string) => {
                 return {
                     search: (query: string, options: { maxFacetHits: number }) => {
-                        if (query === "") return new Promise<{ data: { hits: string[] } }>((resolve) => resolve({ data: { hits: [] } }));
-                        if (query === "hello world") return new Promise<{ data: { hits: string[] } }>((resolve) => resolve({ data: { hits: ["hello", "world"] } }));
-                        if (query === "test") return new Promise<{ data: { hits: string[] } }>((resolve) => resolve({ data: { hits: ["test"] } }));
-                        return new Promise<{ data: { hits: string[] } }>((resolve) => resolve({ data: { hits: ["else"] } }));
+                        if (query === "") return new Promise<{ hits: string[] }>((resolve) => resolve({ hits: ["not", "empty"] }));
+                        if (query === "hello world") return new Promise<{ hits: string[] }>((resolve) => resolve({ hits: ["hello", "world"] }));
+                        if (query === "test") return new Promise<{ hits: string[] }>((resolve) => resolve({ hits: ["test"] }));
+                        return new Promise<{ hits: string[] }>((resolve) => resolve({ hits: ["else"] }));
                     },
                 };
             },
         };
-    };
-});
+    },
+}));
 
 describe("use search nav", () => {
-    it("should should according to the query", () => {
-        const { result } = renderHook(() => useSearchNav<string[]>("", "", ""));
+    it("should should according to the query", async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useSearchNav<string[]>("", "", ""));
 
-        // expect(result.current.selected).toEqual(0);
+        expect(result.current.hits).toEqual([]);
+        expect(result.current.query).toEqual("");
+
+        act(() => result.current.setQuery("test"));
+        await waitForNextUpdate();
+        expect(result.current.hits).toEqual(["test"]);
+        expect(result.current.query).toEqual("test");
+
+        act(() => result.current.setQuery("hello world"));
+        await waitForNextUpdate();
+        expect(result.current.hits).toEqual(["hello", "world"]);
+        expect(result.current.query).toEqual("hello world");
 
         // act(() => result.current.incSelected());
         // expect(result.current.selected).toEqual(1);
-        // expect(result.current.position).toEqual([1000, -1000]);
-
-        // act(() => {
-        //     result.current.decSelected();
-        //     result.current.decSelected();
-        // });
-        // expect(result.current.selected).toEqual(2);
-        // expect(result.current.position).toEqual([-1000, 1000]);
-
-        // act(() => result.current.incSelected());
-        // expect(result.current.selected).toEqual(0);
-        // expect(result.current.position).toEqual([1000, -1000]);
     });
 });
