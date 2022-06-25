ee423f2f8f7d3b26210fd2611248d3ebcfeccc98
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-25 11:10:04 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainQuery.spec.ts
+++ libs/hooks/src/lib/useSearchMainQuery.spec.ts
@@ -1,4 +1,4 @@
-import { SearchHit } from "@facture/types";
+import { Enum_Manufacturer_Color, FindManufacturerCardQuery, SearchHit } from "@facture/types";
 import { renderHook, act } from "@testing-library/react-hooks";
 import { useSearchMainQuery } from "./useSearchMainQuery";
 
@@ -7,12 +7,33 @@ interface Hits {
 }
 
 jest.mock("@apollo/client", () => {
-    const data: SearchHit[] = [];
-    const fetchMore = jest.fn();
+    const data: FindManufacturerCardQuery[] = [
+        {
+            manufacturers: {
+                data: [
+                    {
+                        attributes: {
+                            descriptionShort: "d1",
+                            logo: { data: { attributes: { url: "url1" } } },
+                            manufacturer: "m1",
+                            name: "n1",
+                            type: [{ type: "t1" }],
+                            thumbnail: { data: { attributes: { url: "url1" } } },
+                            color: Enum_Manufacturer_Color["Amber"],
+                        },
+                    },
+                ],
+                meta: { pagination: { pageCount: 2 } },
+            },
+        },
+    ];
+    const fetchMore = (options: { variables: { pageSize: number; page: number } }) => {};
 
-    return { useQuery: jest.fn(() => ({ data, fetchMore })) };
+    return { useQuery: (query: string, options: any) => ({ data, fetchMore }) };
 });
 
-describe("use search main search", () => {
-    it("should return hits according to the query", async () => {});
+describe("use search main query", () => {
+    it("should return hits from the api", async () => {
+        const { result } = renderHook(() => useSearchMainQuery(1));
+    });
 });
