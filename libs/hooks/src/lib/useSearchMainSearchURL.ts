e2ed0108f78f290fedea67f6cc1e8b04d2b75c53
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-26 20:16:29 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainSearchURL.ts
+++ libs/hooks/src/lib/useSearchMainSearchURL.ts
@@ -1,19 +1,23 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function useSearchMainSearchURL(query: string, setQuery: (value: string) => void) {
     const router = useRouter();
-
-    // **** So I want to integrate this into my workflow but I do not want it to continuously update my query param
+    const finished = useRef<boolean>(false);
 
     useEffect(() => {
-        // **** The issue is that this should only load once initially and then it should not load again... - have some sort of checking state to do this ?
+        if (finished.current) return;
 
         const search = router.query["search"];
-        if (search && typeof search === "string") setQuery(decodeURI(search));
+        if (search && typeof search === "string") {
+            finished.current = true;
+            setQuery(decodeURI(search));
+        }
     }, [router]);
 
     useEffect(() => {
+        if (!finished.current) return;
+
         if (query !== "") router.push("?search=" + encodeURI(query));
         else router.push("");
     }, [query]);
