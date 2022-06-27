45592d7bd602c5824fc478d863970a26bc461a6a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 14:01:55 +1000

    🎉 Update

--- libs/components/src/lib/Nav/index.tsx
+++ libs/components/src/lib/Nav/index.tsx
@@ -4,8 +4,8 @@ import { Search } from "./search";
 export function Nav() {
     return (
         <nav className="p-6 mb-14 shadow-md bg-gray-800">
-            <ul className="flex justify-center md:justify-between items-center w-5/6 mx-auto ">
-                <li className="hidden md:block">
+            <ul className="flex flex-col md:flex-row justify-center md:justify-between items-center w-5/6 mx-auto space-y-4 md:space-y-0">
+                <li>
                     <Logo />
                 </li>
                 <li>
