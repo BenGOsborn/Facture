e4bce0a410b88f6681ccdf1b04c36765adafe7f0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 10:54:59 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainSearchURL.spec.ts
+++ libs/hooks/src/lib/useSearchMainSearchURL.spec.ts
@@ -18,29 +18,16 @@ describe("use search main search url", () => {
     it("should return load the query from url and update the main query with it", async () => {
         const setQuery = jest.fn((query: string) => {});
 
-        const { result, rerender } = renderHook(({ query, setQuery }) => useSearchMainSearchURL(query, setQuery), {
+        const { rerender } = renderHook(({ query, setQuery }) => useSearchMainSearchURL(query, setQuery), {
             initialProps: { query: "", setQuery },
         });
 
         expect(setQuery).toHaveBeenCalled();
 
+        // **** How am I going to test that the correct rerender got called ?
+
         rerender({ query: "test 2", setQuery });
 
-        // expect(result.current.query).toEqual("");
-        // expect(result.current.hits).toEqual([]);
-
-        // act(() => result.current.setQuery("test"));
-        // await waitForNextUpdate();
-        // expect(result.current.hits).toEqual(["test"]);
-        // expect(result.current.query).toEqual("test");
-
-        // act(() => result.current.setQuery("hello world"));
-        // await waitForNextUpdate();
-        // expect(result.current.hits).toEqual(["hello", "world"]);
-        // expect(result.current.query).toEqual("hello world");
-
-        // act(() => result.current.setQuery(""));
-        // expect(result.current.hits).toEqual([]);
-        // expect(result.current.query).toEqual("");
+        rerender({ query: "", setQuery });
     });
 });
