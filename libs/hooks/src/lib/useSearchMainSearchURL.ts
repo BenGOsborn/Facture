d4d2e6e521d9921a97ea210a5d11984cb37be6e2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 10:24:27 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainSearchURL.ts
+++ libs/hooks/src/lib/useSearchMainSearchURL.ts
@@ -5,6 +5,8 @@ export function useSearchMainSearchURL(query: string, setQuery: (value: string) 
     const router = useRouter();
     const finished = useRef<boolean>(false);
 
+    // **** Now we have a problem where if the type is never found it will just not work ever
+
     useEffect(() => {
         if (finished.current) return;
 
@@ -16,6 +18,8 @@ export function useSearchMainSearchURL(query: string, setQuery: (value: string) 
     }, [router]);
 
     useEffect(() => {
+        console.log(finished.current);
+
         if (!finished.current) return;
 
         if (query !== "") router.push("?search=" + encodeURI(query));
