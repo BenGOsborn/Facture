44cb9b751065167dc8a7f6b7e13f3db917e49c87
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 10:50:25 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainSearchURL.spec.ts
+++ libs/hooks/src/lib/useSearchMainSearchURL.spec.ts
@@ -8,7 +8,7 @@ jest.mock("next/router", () => {
         __esModule: true,
         ...originalModule,
         useRouter: () => ({
-            query: { search: "hello world" },
+            query: { search: "test 1" },
         }),
     };
 });
@@ -17,12 +17,15 @@ describe("use search main search url", () => {
     it("should return load the query from url and update the main query with it", async () => {
         const setQuery = jest.fn((query: string) => {});
 
-        const { result, waitForNextUpdate, rerender } = renderHook(({ query, setQuery }) => useSearchMainSearchURL(query, setQuery), {
+        const { result, rerender } = renderHook(({ query, setQuery }) => useSearchMainSearchURL(query, setQuery), {
             initialProps: { query: "", setQuery },
         });
 
-        // await waitForNextUpdate();
         expect(setQuery).toHaveBeenCalled();
+
+        console.log(result);
+
+        rerender({ query: "test 2", setQuery });
 
         // expect(result.current.query).toEqual("");
         // expect(result.current.hits).toEqual([]);
