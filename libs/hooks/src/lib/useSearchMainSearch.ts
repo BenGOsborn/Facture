c38c516a86aa04b6f05bca2bf4be2b6adda92f3b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-27 13:05:21 +1000

    🎉 Update

--- libs/hooks/src/lib/useSearchMainSearch.ts
+++ libs/hooks/src/lib/useSearchMainSearch.ts
@@ -2,7 +2,9 @@ import { SearchHit } from "@facture/types";
 import { parseAlgoliaSearchHits } from "@facture/graphql";
 import algoliasearch from "algoliasearch";
 import { useEffect, useState } from "react";
+
 import useSearchMainSearchURL from "./useSearchMainSearchURL";
+import useLocation from "./useLocation";
 
 export function useSearchMainSearch(algoliaAppId: string, algoliaApiKey: string, algoliaIndexName: string, pageSize: number) {
     const searchClient = algoliasearch(algoliaAppId, algoliaApiKey);
@@ -13,6 +15,8 @@ export function useSearchMainSearch(algoliaAppId: string, algoliaApiKey: string,
     const [pageCount, setPageCount] = useState<number>(-1);
     const [currentPage, setCurrentPage] = useState<number>(0);
 
+    const location = useLocation();
+
     useSearchMainSearchURL(query, setQuery);
 
     const loadMore = () => setCurrentPage((page) => page + 1);
@@ -20,23 +24,25 @@ export function useSearchMainSearch(algoliaAppId: string, algoliaApiKey: string,
     useEffect(() => {
         if (query === "") setData(null);
         else
-            index.search(query, { hitsPerPage: pageSize, page: 0 }).then((data) => {
+            index.search(query, { aroundLatLng: location ? `${location.latitude},${location.longitude}` : undefined, hitsPerPage: pageSize, page: 0 }).then((data) => {
                 setPageCount(data.nbPages);
                 setData(parseAlgoliaSearchHits(data.hits));
             });
 
         setCurrentPage(0);
-    }, [query, pageSize]);
+    }, [query, pageSize, location]);
 
     useEffect(() => {
         if (currentPage > 0)
-            index.search(query, { hitsPerPage: pageSize, page: currentPage }).then((data) =>
-                setData((prev) => {
-                    const parsed = parseAlgoliaSearchHits(data.hits);
-                    return prev ? [...prev, ...parsed] : parsed;
-                })
-            );
-    }, [currentPage]);
+            index
+                .search(query, { aroundLatLng: location ? `${location.latitude},${location.longitude}` : undefined, hitsPerPage: pageSize, page: currentPage })
+                .then((data) =>
+                    setData((prev) => {
+                        const parsed = parseAlgoliaSearchHits(data.hits);
+                        return prev ? [...prev, ...parsed] : parsed;
+                    })
+                );
+    }, [currentPage, location]);
 
     return {
         data,
