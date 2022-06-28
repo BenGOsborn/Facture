8d69574ef1bcd0ac00af8a39bab960b026fdb3f9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-28 13:45:49 +1000

    🎉 Update

--- libs/helpers/src/lib/ga.ts
+++ libs/helpers/src/lib/ga.ts
@@ -13,3 +13,10 @@ export function emitSectionHover(section: "contact" | "info" | "content" | "disp
         // @ts-ignore
         window.gtag("event", "section_hover", { section, period });
 }
+
+export function emitCardHover(card: "nav_search" | "landing_search", period: number) {
+    if (window !== undefined)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        window.gtag("event", "card_hover", { card, period });
+}
