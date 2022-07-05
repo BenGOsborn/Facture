1b0e49aca49e91e6d6e708f53e281f2ae83f4a95
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-05 12:28:58 +1000

    🎉 Update

--- libs/hooks/src/lib/useDelay.ts
+++ libs/hooks/src/lib/useDelay.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
 export function useDelay(trigger: any, callback: () => void, delay: number) {
-    let timeout = useRef<NodeJS.Timeout | null>(null);
+    const timeout = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
         if (timeout.current) clearTimeout(timeout.current);
