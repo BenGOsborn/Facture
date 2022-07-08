b7a22a9c5cdd04050a5ad1eaaa7a9ff34e754136
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-08 14:29:05 +1000

    🎉 Update

--- libs/components/src/lib/Logo/index.tsx
+++ libs/components/src/lib/Logo/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Logo() {
     return (
-        <Link href="/">
+        <Link href="/search">
             <a className="flex flex-col md:items-start items-center">
                 <p className="font-bold text-3xl text-white">Facture</p>
                 <p className="font-bold text-gray-400 text-sm">Connecting Manufacturers</p>
