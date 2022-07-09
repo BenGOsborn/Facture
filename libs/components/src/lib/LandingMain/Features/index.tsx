de7d4aa2fd914c2b6430ec81c33d15f619412ff4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-09 20:26:57 +1000

    🎉 Update

--- libs/components/src/lib/LandingMain/Features/index.tsx
+++ libs/components/src/lib/LandingMain/Features/index.tsx
@@ -1,42 +1,23 @@
-import { BatteryOff, CreditCardOff, UserOff } from "tabler-icons-react";
-import { RainbowText } from "../../RainbowText";
 import { Feature } from "./feature";
 
-export function Features() {
-    const features: {
+interface Props {
+    heading: string | JSX.Element;
+    subheading: string | JSX.Element;
+    description: string | JSX.Element;
+    features: {
         title: string;
         description: string;
         icon: JSX.Element;
-    }[] = [
-        {
-            title: "100% Free.",
-            description:
-                "Getting your business listed on our platform is 100% free. Our mission is to create a comprehensive registry for entrepreneurs, and as such, registering will remain free forever.",
-            icon: <CreditCardOff />,
-        },
-        {
-            title: "No Sign Up.",
-            description:
-                "Getting your business listed on our platform requires absolutely no sign up process. Simply add your businesses information to our sign up form and we'll list you ASAP!",
-            icon: <UserOff />,
-        },
-        { title: "Completely Hands Off.", description: "Getting your business listed on our platform requires absolutely no sign up process.", icon: <BatteryOff /> },
-    ];
+    }[];
+}
 
-    // **** This is advantages, we need another one for the benefits provided...
-
-    // **** Benefits provided to businesses and entrepreneurs (free web page, search ready from day one)
-
+export function Features({ heading, subheading, description, features }: Props) {
     return (
         <div className="text-center space-y-14">
             <div className="space-y-3 lg:w-2/5 lg:mx-auto">
-                <p className="text-gray-700 font-bold text-md">Advantages</p>
-                <h3 className="text-gray-900 font-bold text-3xl">
-                    <RainbowText>Advantages</RainbowText> Of Our Platform
-                </h3>
-                <p className="text-gray-700 font-medium text-lg">
-                    Here's a few advantages that using our platform provides your manufacturing business in addition to other platforms.
-                </p>
+                <p className="text-gray-700 font-bold text-md">{subheading}</p>
+                <h3 className="text-gray-900 font-bold text-3xl">{heading}</h3>
+                <p className="text-gray-700 font-medium text-lg">{description}</p>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 auto-rows-max auto-cols-max mx-auto w-4/5">
                 {features.map((feature, index) => (
--- libs/components/src/lib/LandingMain/Hero/index.tsx
+++ libs/components/src/lib/LandingMain/Hero/index.tsx
@@ -6,24 +6,29 @@ import { Search } from "./search";
 export function Hero() {
     return (
         <header className="text-center space-y-14 xl:mx-auto xl:w-3/5">
-            <h1 className="text-6xl md:text-7xl xl:text-8xl font-bold text-gray-900">
-                Connecting <RainbowText>Manufacturers</RainbowText>. Driving <RainbowText>Innovation</RainbowText>.
+            <h1 className="text-6xl md:text-8xl font-bold text-gray-900 flex flex-col items-center">
+                <span>Connecting</span>
+                <span>
+                    <RainbowText>Manufacturers</RainbowText>.
+                </span>
+                <span>Driving</span>
+                <span>
+                    <RainbowText>Innovation</RainbowText>.
+                </span>
             </h1>
-            <div className="space-y-3">
-                <ul className="flex flex-col md:flex-row items-stretch md:items-center justify-center space-x-0 md:space-x-7 space-y-3 md:space-y-0">
-                    <div className="md:w-2/3">
-                        <Search />
-                    </div>
-                    <Link href="https://forms.gle/59gjvMtDMJ6rmDbS7">
-                        <a className="p-6 bg-gray-300 hover:bg-gray-400 transition-colors text-lg rounded-md font-medium text-gray-700 md:w-1/3 min-w-max">
-                            Register Your Business
-                        </a>
-                    </Link>
-                </ul>
-                <p className="text-left text-gray-700 font-medium text-lg">Completely free. No sign up required.</p>
-            </div>
+            <ul className="flex flex-col md:flex-row items-stretch md:items-center justify-center space-x-0 md:space-x-7 space-y-7 md:space-y-0">
+                <div className="md:w-2/3">
+                    <Search />
+                </div>
+                <Link href="https://forms.gle/59gjvMtDMJ6rmDbS7">
+                    <a className="p-6 bg-gray-200 hover:bg-gray-300 transition-colors text-lg rounded-md font-medium text-gray-700 md:w-1/3 min-w-max">
+                        Register Your Business
+                    </a>
+                </Link>
+            </ul>
             <h2 className="text-xl font-medium text-gray-700">
-                Facture is an online search directory connecting manufacturers to aspiring entrepreneurs to drive local innovation.
+                Facture is an online search directory for manufacturers with the intention of connecting manufacturers to aspiring entrepreneurs to drive local
+                innovation.
             </h2>
         </header>
     );
