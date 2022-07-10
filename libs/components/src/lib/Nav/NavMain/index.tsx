51bb6b46b593a67f27572b400fc5a8bcb844f4ab
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-10 15:05:12 +1000

    🎉 Update

--- libs/components/src/lib/Nav/NavMain/index.tsx
+++ libs/components/src/lib/Nav/NavMain/index.tsx
@@ -6,7 +6,7 @@ export function NavMain() {
     return (
         <nav className="p-6 bg-gray-800 space-y-10">
             <ul className="flex flex-col md:flex-row justify-center md:justify-between items-center w-5/6 mx-auto space-y-3 md:space-y-0 md:space-x-3">
-                <li className="w-full md:w-1/6">
+                <li className="w-full md:w-2/3">
                     <Logo />
                 </li>
                 <li className="w-full md:w-1/3 min-w-max">
