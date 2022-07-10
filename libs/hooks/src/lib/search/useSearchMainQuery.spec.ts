efcd3621bed0d50a2d45813c178afdfcecdaf5e5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-10 11:29:43 +1000

    🎉 Update

--- libs/hooks/src/lib/search/useSearchMainQuery.spec.ts
+++ libs/hooks/src/lib/search/useSearchMainQuery.spec.ts
@@ -3,7 +3,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import { useSearchMainQuery } from "./useSearchMainQuery";
 
-const data: FindManufacturerCardQueryType[] = [
+const testData: FindManufacturerCardQueryType[] = [
     {
         manufacturers: {
             data: [
@@ -48,54 +48,45 @@ jest.mock("@apollo/client", () => {
     const originalModule = jest.requireActual("@apollo/client");
 
     const fetchMore = (options: { variables: { pageSize: number; page: number } }) =>
-        new Promise<{ data: FindManufacturerCardQueryType }>((resolve) => resolve({ data: data[1] }));
+        new Promise<{ data: FindManufacturerCardQueryType }>((resolve) => resolve({ data: testData[1] }));
 
-    return { __esModule: true, ...originalModule, useQuery: (query: string, options: any) => ({ data: data[0], fetchMore }) };
+    return { __esModule: true, ...originalModule, useQuery: (query: string, options: any) => ({ data: testData[0], fetchMore }) };
 });
 
 describe("use search main query", () => {
     it("should return hits from the api", async () => {
+        const expected = [
+            {
+                descriptionShort: testData[0].manufacturers.data[0].attributes.descriptionShort,
+                logo: { url: testData[0].manufacturers.data[0].attributes.logo.data.attributes.url },
+                manufacturer: testData[0].manufacturers.data[0].attributes.manufacturer,
+                name: testData[0].manufacturers.data[0].attributes.name,
+                slogan: testData[0].manufacturers.data[0].attributes.slogan,
+                type: testData[0].manufacturers.data[0].attributes.type?.map((type) => type.type),
+                color: testData[0].manufacturers.data[0].attributes.color,
+                thumbnail: { url: testData[0].manufacturers.data[0].attributes.thumbnail.data.attributes.url },
+            },
+            {
+                descriptionShort: testData[1].manufacturers.data[0].attributes.descriptionShort,
+                logo: { url: testData[1].manufacturers.data[0].attributes.logo.data.attributes.url },
+                manufacturer: testData[1].manufacturers.data[0].attributes.manufacturer,
+                name: testData[1].manufacturers.data[0].attributes.name,
+                slogan: testData[1].manufacturers.data[0].attributes.slogan,
+                type: testData[1].manufacturers.data[0].attributes.type?.map((type) => type.type),
+                color: testData[1].manufacturers.data[0].attributes.color,
+                thumbnail: { url: testData[1].manufacturers.data[0].attributes.thumbnail.data.attributes.url },
+            },
+        ];
+
         const { result, waitForNextUpdate } = renderHook(() => useSearchMainQuery(1));
 
-        expect(result.current.data).toEqual([
-            {
-                descriptionShort: "d1",
-                logo: { url: "url1" },
-                manufacturer: "m1",
-                name: "n1",
-                slogan: null,
-                type: ["t1"],
-                color: "amber",
-                thumbnail: { url: "url1" },
-            },
-        ]);
+        expect(result.current.data).toEqual([expected[0]]);
 
         act(() => {
             if (result.current.loadMore) result.current.loadMore();
         });
         await waitForNextUpdate();
-        expect(result.current.data).toEqual([
-            {
-                descriptionShort: "d1",
-                logo: { url: "url1" },
-                manufacturer: "m1",
-                name: "n1",
-                slogan: null,
-                type: ["t1"],
-                color: "amber",
-                thumbnail: { url: "url1" },
-            },
-            {
-                descriptionShort: "d2",
-                logo: { url: "url2" },
-                manufacturer: "m2",
-                name: "n2",
-                slogan: null,
-                type: ["t2"],
-                color: "blue",
-                thumbnail: { url: "url2" },
-            },
-        ]);
+        expect(result.current.data).toEqual(expected);
 
         expect(result.current.loadMore).not.toBeTruthy();
     });
