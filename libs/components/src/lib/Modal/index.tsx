1cb8848f36fcaffd5b0b369dc0ac3ebbc4b4650a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-10 22:22:33 +1000

    🎉 Update

--- libs/components/src/lib/Modal/index.tsx
+++ libs/components/src/lib/Modal/index.tsx
@@ -13,19 +13,17 @@ export function Modal({ title, onClose, children }: Props) {
 
     return mounted
         ? createPortal(
-              <>
-                  <div className="fixed inset-0 bg-black bg-opacity-60 transition-opacity flex items-center justify-center">
-                      <div className="bg-gray-100 p-6 rounded-md flex-col w-5/6 md:w-1/2 space-y-3">
-                          <div className="flex items-center justify-between text-gray-900 font-medium text-lg">
-                              {title}
-                              <span className="cursor-pointer" onClick={onClose}>
-                                  <X />
-                              </span>
-                          </div>
-                          <div>{children}</div>
+              <div className="modal absolute inset-0 bg-black bg-opacity-60 transition-opacity flex items-center justify-center">
+                  <div className="bg-gray-100 p-6 rounded-md flex-col w-5/6 md:w-1/2 space-y-3">
+                      <div className="flex items-center justify-between text-gray-900 font-medium text-lg">
+                          {title}
+                          <span className="cursor-pointer" onClick={onClose}>
+                              <X />
+                          </span>
                       </div>
+                      <div>{children}</div>
                   </div>
-              </>,
+              </div>,
               document.querySelector("#modal") as Element
           )
         : null;
