b995bf86814f60dcceb1cbac2a444e1d2990107a
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-11 18:33:50 +1000

    🎉 Update

--- libs/components/src/lib/LandingMain/About/index.tsx
+++ libs/components/src/lib/LandingMain/About/index.tsx
@@ -2,23 +2,24 @@ import { RainbowText } from "../../RainbowText";
 
 export function About() {
     return (
-        <div className="text-center space-y-7">
+        <div className="text-center space-y-14">
             <div className="space-y-3">
-                <p className="text-gray-700 font-bold text-xl">About Us</p>
-                <h2 className="text-4xl font-bold text-gray-900">
+                <p className="text-gray-700 font-bold text-md">About Us</p>
+                <h2 className="text-3xl font-bold text-gray-900">
                     What Is <RainbowText>Facture</RainbowText>?
                 </h2>
             </div>
-            <div className="space-y-7 text-gray-700 text-xl font-medium xl:w-2/3 xl:mx-auto">
-                <p>
+            <div></div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-7 auto-rows-max auto-cols-max mx-auto w-4/5">
+                <p className="font-medium text-md text-gray-700">
                     Facture was created to provide manufacturers with a simple all-in-one platform where they can advertise their services and be easily found by
                     entrepreneurs.
                 </p>
-                <p>
+                <p className="font-medium text-md text-gray-700">
                     Our mission is to drive local industry and innovation with our easy to use smart manufacturer search, helping entrepreneurs to find suppliers with
                     ease.
                 </p>
-                <p>
+                <p className="font-medium text-md text-gray-700">
                     We understand that you as a business owner have a lot to deal with, so we take care of increasing your digital presence so that you don't waste your
                     time, money, or brain power.
                 </p>
