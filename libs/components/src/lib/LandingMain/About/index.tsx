bd3e18418bc3e2bb6da8787092717e2e2241f1d2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-11 19:45:58 +1000

    🎉 Update

--- libs/components/src/lib/LandingMain/About/index.tsx
+++ libs/components/src/lib/LandingMain/About/index.tsx
@@ -2,26 +2,25 @@ import { RainbowText } from "../../RainbowText";
 
 export function About() {
     return (
-        <div className="text-center space-y-14">
-            <div className="space-y-3">
+        <div className="space-y-7">
+            <div className="text-center space-y-3">
                 <p className="text-gray-700 font-bold text-md">About Us</p>
                 <h2 className="text-3xl font-bold text-gray-900">
                     What Is <RainbowText>Facture</RainbowText>?
                 </h2>
             </div>
-            <div></div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-7 auto-rows-max auto-cols-max mx-auto w-4/5">
+            <div className="text-left grid grid-cols-1 md:grid-cols-2 gap-7 auto-rows-max auto-cols-max mx-auto w-4/5 xl:w-3/5">
                 <p className="font-medium text-md text-gray-700">
-                    Facture was created to provide manufacturers with a simple all-in-one platform where they can advertise their services and be easily found by
-                    entrepreneurs.
+                    Facture was created to provide manufacturers with a <span className="text-gray-900">simple all-in-one</span> platform where they can advertise their
+                    services and be easily found by entrepreneurs.
                 </p>
                 <p className="font-medium text-md text-gray-700">
-                    Our mission is to drive local industry and innovation with our easy to use smart manufacturer search, helping entrepreneurs to find suppliers with
-                    ease.
+                    Our mission is to <span className="text-gray-900">drive local industry</span> and innovation with our easy to use smart manufacturer search, helping
+                    entrepreneurs to <span className="text-gray-900">find suppliers with ease</span>.
                 </p>
                 <p className="font-medium text-md text-gray-700">
-                    We understand that you as a business owner have a lot to deal with, so we take care of increasing your digital presence so that you don't waste your
-                    time, money, or brain power.
+                    We understand that you as a business owner have a lot to deal with, so we take care of{" "}
+                    <span className="text-gray-900">increasing your digital presence</span> so that you don't waste your time, money, or brain power.
                 </p>
             </div>
         </div>
