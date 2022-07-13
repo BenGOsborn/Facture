b6e15783c9fbab97d84d87e0d3ff09f47b54b270
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-13 20:01:21 +1000

    🎉 Update

--- libs/helpers/src/lib/constants.ts
+++ libs/helpers/src/lib/constants.ts
@@ -1,14 +1,11 @@
 export const ANALYTICS_HOVER_PERIOD = 750;
-
 export const SEARCH_DELAY = 750;
-
 export const PROMOTION_DELAY = 30 * 1000;
 
 export const NAME = "Facture";
 export const SLOGAN = "Connecting Aussie Manufacturers";
 export const DESCRIPTION = "Facture is an online platform and search directory connecting Aussie manufacturers to entrepreneurs to drive local industry and innovation.";
+
 export const URL = "https://facture.com.au";
-
 export const FORM_URL = "https://form.jotform.com/221923021435041";
-
 export const PRIVACY_POLICY_URL = "https://amused-fairy-792.notion.site/Facture-Privacy-Policy-d6dbb276f3664bb78c12694e1d3a804e";
