6fa40c82bee6cd36be204601a226220ab2425956
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-14 12:41:38 +1000

    🎉 Update

--- apps/total-page-views/src/main.spec.ts
+++ apps/total-page-views/src/main.spec.ts
@@ -1,7 +1,16 @@
 import { handler } from "./main";
+import { z } from "zod";
 
 describe("main", () => {
-    it("should work", async () => {
-        await handler({} as any);
+    it("should get the number of views for a given manufacturer", async () => {
+        const result = await handler({ pathParameters: { manufacturer: "mcm-manufacturing" }, queryStringParameters: { from: "0" } } as any);
+        result.body = JSON.parse(result.body);
+
+        const schema = z.object({
+            body: z.object({
+                views: z.number(),
+            }),
+        });
+        expect(schema.safeParse(result).success).toEqual(true);
     });
 });
--- apps/total-page-views/src/main.ts
+++ apps/total-page-views/src/main.ts
@@ -2,8 +2,8 @@ import { BigQuery } from "@google-cloud/bigquery";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-    // const fileName = event.pathParameters?.file;
-    // const size = event.queryStringParameters?.size;
+    const manufacturer = event.pathParameters?.manufacturer;
+    const from = parseInt(event.queryStringParameters?.from);
 
     const creds = JSON.parse(process.env.GCP_CREDENTIALS);
     const region = process.env.GCP_REGION;
@@ -16,27 +16,21 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
         },
     });
 
-    console.log("Hello1");
-
     const query = `
-    SELECT
-  key,
-  value.string_value
-FROM
-  \`facture-353822.analytics_320496772.*\`,
-  UNNEST(event_params)
-WHERE
-  event_name = "card_search_hit"
-  AND key = "cardOwnerId"
-  OR key = "query"
-LIMIT
-  100
-    `;
+                    SELECT
+                        COUNT(*) as views
+                    FROM
+                        \`facture-353822.analytics_320496772.*\`,
+                        UNNEST(event_params)
+                    WHERE
+                        event_name = "page_view"
+                        AND key = "page_location"
+                        AND value.string_value LIKE CONCAT("%facture.com.au%", @manufacturer, "%")
+                        AND event_timestamp >= @from
+                `;
 
-    const [job] = await bq.createQueryJob({ query, location: region });
-    const [rows] = await job.getQueryResults();
+    const [job] = await bq.createQueryJob({ query, location: region, params: { manufacturer, from } });
+    const [[row]] = await job.getQueryResults();
 
-    console.log(rows);
-
-    return { body: JSON.stringify("Hello world"), statusCode: 200 };
+    return { body: JSON.stringify(row), statusCode: 200 };
 };
